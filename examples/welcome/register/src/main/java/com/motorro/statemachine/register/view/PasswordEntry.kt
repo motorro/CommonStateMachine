2b18f3d839318a81d69d817a8eec2c65949515dc
motorro <motorro@gmail.com>
2023-10-02 18:20:14 +0200

    Examples tidy-up Fixes: #8

---
+++ examples/welcome/register/src/main/java/com/motorro/statemachine/register/view/PasswordEntry.kt
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2022 Nikolai Kotchetkov.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.motorro.statemachine.register.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.motorro.statemachine.register.R
+import com.motorro.statemachine.register.data.RegisterUiState
+
+@Composable
+fun PasswordEntry(
+    state: RegisterUiState.PasswordEntry,
+    onPasswordChanged: (String) -> Unit,
+    onRepeatPasswordChanged: (String) -> Unit,
+    onPrevious: () -> Unit,
+    onNext: () -> Unit
+) {
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier
+        .verticalScroll(scrollState)
+        .fillMaxSize()
+        .statusBarsPadding()
+        .imePadding()
+        .navigationBarsPadding()
+        .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = stringResource(id = R.string.register_password_entry_title),
+                style = MaterialTheme.typography.h4
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = state.email,
+                style = MaterialTheme.typography.h3
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = stringResource(id = R.string.register_password_entry_desc),
+                style = MaterialTheme.typography.body1
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = state.password,
+                onValueChange = onPasswordChanged,
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(stringResource(id = R.string.register_password_entry_label)) },
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = state.repeatPassword,
+                onValueChange = onRepeatPasswordChanged,
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(stringResource(id = R.string.register_repeat_password_entry_label)) },
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            val validationError = state.validationError
+            if (null != validationError) {
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Text(
+                    color = MaterialTheme.colors.error,
+                    text = validationError
+                )
+            }
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(onClick = onPrevious) {
+                Text(text = stringResource(id = R.string.register_password_entry_prev))
+            }
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Button(onClick = onNext) {
+                Text(text = stringResource(id = R.string.register_password_entry_next))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun EmailEntryPreview() {
+    PasswordEntry(
+        state = RegisterUiState.PasswordEntry(
+            email = "test@example.com",
+            password = "123456",
+            repeatPassword = "123456",
+            validationError = "Too complex password"
+        ),
+        onPasswordChanged = {},
+        onRepeatPasswordChanged = {},
+        onPrevious = {},
+        onNext = {}
+    )
+}
---
+++ examples/welcome/welcome/src/main/java/com/motorro/statemachine/welcome/view/EmailEntry.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2022 Nikolai Kotchetkov.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.motorro.statemachine.welcome.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.motorro.statemachine.R
+import com.motorro.statemachine.welcome.data.WelcomeUiState
+
+@Composable
+fun EmailEntry(state: WelcomeUiState.EmailEntry, onEmailChanged: (String) -> Unit, onNext: () -> Unit) {
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier
+        .verticalScroll(scrollState)
+        .fillMaxSize()
+        .statusBarsPadding()
+        .imePadding()
+        .navigationBarsPadding()
+        .padding(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = stringResource(id = R.string.email_title),
+                style = MaterialTheme.typography.h4
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = stringResource(id = R.string.email_desc),
+                style = MaterialTheme.typography.body1
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = state.email,
+                onValueChange = onEmailChanged,
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(stringResource(id = R.string.email_email_label)) }
+            )
+        }
+
+        Button(onClick = onNext, enabled = state.actionEnabled) {
+            Text(text = stringResource(id = R.string.welcome_next))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun EmailEntryPreview() {
+
+    EmailEntry(
+        state = WelcomeUiState.EmailEntry(
+            email = "test@example.com",
+            actionEnabled = true
+        ),
+        onEmailChanged = {},
+        onNext = {}
+    )
+}
